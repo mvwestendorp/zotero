af08b8f403d018fed6c28dd6787d7844c123dcfe
Frank Bennett <biercenator@gmail.com>
2014-04-27 21:19:44 +0900

    Fix indenting

--- chrome/content/zotero/bibliography.js
+++ chrome/content/zotero/bibliography.js
@@ -173,18 +173,18 @@ var Zotero_File_Interface_Bibliography = new function() {
 		var citationPrefNames = ['Persons', 'Institutions', 'Titles', 'Journals', 'Publishers', 'Places'];
 		for (var i = 0, ilen = citationPrefNames.length; i < ilen; i += 1) {
 			var prefname = citationPrefNames[i].toLowerCase();
-            var citationRoleNames = ["orig","translit","translat"];
+			var citationRoleNames = ["orig","translit","translat"];
 			citationLangSet(citationPrefNames[i], true);
-            for (var j = 0, jlen = citationRoleNames.length; j < jlen; j += 1) {
-                var rolename = citationRoleNames[j];
-			    var citationPrefNode = document.getElementById(prefname + '-radio-orig');
-                if (citationPrefNode) {
-				    if (_io['citationLangPrefs'+citationPrefNames[i]] && _io['citationLangPrefs'+citationPrefNames[i]].length) {
-					    var selectedCitationPrefNode = document.getElementById(prefname + "-radio-" + _io['citationLangPrefs'+citationPrefNames[i]][0]);
-					    selectedCitationPrefNode.checked = true;
-				    }
-                }
-            }
+			for (var j = 0, jlen = citationRoleNames.length; j < jlen; j += 1) {
+				var rolename = citationRoleNames[j];
+				var citationPrefNode = document.getElementById(prefname + '-radio-orig');
+				if (citationPrefNode) {
+					if (_io['citationLangPrefs'+citationPrefNames[i]] && _io['citationLangPrefs'+citationPrefNames[i]].length) {
+						var selectedCitationPrefNode = document.getElementById(prefname + "-radio-" + _io['citationLangPrefs'+citationPrefNames[i]][0]);
+						selectedCitationPrefNode.checked = true;
+					}
+				}
+			}
 		}
 
 		var langPrefs = document.getElementById('lang-prefs');
@@ -201,17 +201,17 @@ var Zotero_File_Interface_Bibliography = new function() {
 					'citationSort'
 				];
 				for (var j = 0, jlen = langSelectorTypes.length; j < jlen; j += 1) {
-		            var newselector = buildSelector('default',tags[i],langSelectorTypes[j]);
-		            if ((j % 3) == 0) {
-			            newselector.setAttribute("class", "translit");
-                        newselector.setAttribute("onmouseover", "Zotero_File_Interface_Bibliography.setLanguageRoleHighlight(['translit-primary', 'translit-secondary', 'translit'],true);");
-                        newselector.setAttribute("onmouseout", "Zotero_File_Interface_Bibliography.setLanguageRoleHighlight(['translit-primary', 'translit-secondary', 'translit'],false);");
-		            } else if ((j % 3) == 1) {
-			            newselector.setAttribute("class", "translat");
-                        newselector.setAttribute("onmouseover", "Zotero_File_Interface_Bibliography.setLanguageRoleHighlight(['translat-primary', 'translat-secondary', 'translat'],true);");
-                        newselector.setAttribute("onmouseout", "Zotero_File_Interface_Bibliography.setLanguageRoleHighlight(['translat-primary', 'translat-secondary', 'translat'],false);");
-		            }
-		            langSelectors.push(newselector);
+					var newselector = buildSelector('default',tags[i],langSelectorTypes[j]);
+					if ((j % 3) == 0) {
+						newselector.setAttribute("class", "translit");
+						newselector.setAttribute("onmouseover", "Zotero_File_Interface_Bibliography.setLanguageRoleHighlight(['translit-primary', 'translit-secondary', 'translit'],true);");
+						newselector.setAttribute("onmouseout", "Zotero_File_Interface_Bibliography.setLanguageRoleHighlight(['translit-primary', 'translit-secondary', 'translit'],false);");
+					} else if ((j % 3) == 1) {
+						newselector.setAttribute("class", "translat");
+						newselector.setAttribute("onmouseover", "Zotero_File_Interface_Bibliography.setLanguageRoleHighlight(['translat-primary', 'translat-secondary', 'translat'],true);");
+						newselector.setAttribute("onmouseout", "Zotero_File_Interface_Bibliography.setLanguageRoleHighlight(['translat-primary', 'translat-secondary', 'translat'],false);");
+					}
+					langSelectors.push(newselector);
 				}
 				addSelectorRow(langPrefs,langSelectors);
 			}
@@ -321,31 +321,31 @@ var Zotero_File_Interface_Bibliography = new function() {
 		target.appendChild(row);
 	}
 		
-    function setLanguageRoleHighlight(classes, mode) {
-	    for (var i = 0, ilen = classes.length; i < ilen; i += 1) {
-		    var nodes = document.getElementsByClassName(classes[i]);
-		    for (var j = 0, jlen = nodes.length; j < jlen; j += 1) {
-                var lst;
-			    var str = nodes[j].getAttribute("class");
-			    if (str) {
-				    lst = str.split(/\s+/);
-			    } else {
-				    lst = [];
-			    }
-			    if (mode) {
-				    lst.push("language-role-highlight");
-				    nodes[j].setAttribute("class", lst.join(" "));
-			    } else {
-                    for (var k = lst.length - 1; k > -1; k += -1) {
-                        if (lst[k] === "language-role-highlight") {
-                            lst = lst.slice(0, k).concat(lst.slice(k + 1));
-                        }
-                    }
-                    nodes[j].setAttribute("class", lst.join(" "));
-                }
-		    }
-	    }
-    };
+	function setLanguageRoleHighlight(classes, mode) {
+		for (var i = 0, ilen = classes.length; i < ilen; i += 1) {
+			var nodes = document.getElementsByClassName(classes[i]);
+			for (var j = 0, jlen = nodes.length; j < jlen; j += 1) {
+				var lst;
+				var str = nodes[j].getAttribute("class");
+				if (str) {
+					lst = str.split(/\s+/);
+				} else {
+					lst = [];
+				}
+				if (mode) {
+					lst.push("language-role-highlight");
+					nodes[j].setAttribute("class", lst.join(" "));
+				} else {
+					for (var k = lst.length - 1; k > -1; k += -1) {
+						if (lst[k] === "language-role-highlight") {
+							lst = lst.slice(0, k).concat(lst.slice(k + 1));
+						}
+					}
+					nodes[j].setAttribute("class", lst.join(" "));
+				}
+			}
+		}
+	};
 
 	function buildSelector (profile,tagdata,param) {
 		//Zotero.debug("XXX == buildSelector() ==");
@@ -392,22 +392,22 @@ var Zotero_File_Interface_Bibliography = new function() {
 		}
 	}
 
-    function capFirst(str) {
-	    return str[0].toUpperCase() + str.slice(1);
-    }
+	function capFirst(str) {
+		return str[0].toUpperCase() + str.slice(1);
+	}
 
-    function citationPrimary(node) {
-	    var lst = node.getAttribute("id").split('-');
-	    var base = lst[0];
-        var primarySetting = lst[2];
+	function citationPrimary(node) {
+		var lst = node.getAttribute("id").split('-');
+		var base = lst[0];
+		var primarySetting = lst[2];
 		var settings = _io['citationLangPrefs'+capFirst(base)];
-        if (!settings) {
-            settings = ['orig'];
-        }
-	    _io['citationLangPrefs'+capFirst(base)] = [primarySetting].concat(settings.slice(1));
-        // Second true is for a radio click
-	    citationLangSet(capFirst(base), true, true);
-    }
+		if (!settings) {
+			settings = ['orig'];
+		}
+		_io['citationLangPrefs'+capFirst(base)] = [primarySetting].concat(settings.slice(1));
+		// Second true is for a radio click
+		citationLangSet(capFirst(base), true, true);
+	}
 
 	function citationSecondary(node) {
 		//Zotero.debug("XXX == citationSecondary() ==");
@@ -449,107 +449,107 @@ var Zotero_File_Interface_Bibliography = new function() {
 		}
 	};
 
-    function citationLangSet (name, init, radioClick) {
+	function citationLangSet (name, init, radioClick) {
 		var settings = _io['citationLangPrefs'+name];
-	    if (!settings || !settings[0]) {
-		    settings = ['orig'];
-	    }
-	    var nodes = [];
-	    var forms = ['orig', 'translit', 'translat'];
-        var base = name.toLowerCase();
-        // get node
-        // set node from pref
-        if (init) {
-            citationGetAffixes();
-            var currentPrimaryID = base + "-radio-" + settings[0];
-            var node = document.getElementById(currentPrimaryID);
-            var control = node.control;
-            control.selectedItem = node;
-            
-            var translitID = base + "-radio-translit";
-            var translitNode = document.getElementById(translitID);
-            nodes.push(translitNode);
-            
-            for (var i = 0, ilen = forms.length; i < ilen; i += 1) {
-                nodes.push(document.getElementById(base + "-checkbox-" + forms[i]));
-            }
-	        for (var i = 0, ilen = nodes.length; i < ilen; i += 1) {
-		        nodes[i].checked = false;
-		        for (var j = 1, jlen = settings.length; j < jlen; j += 1) {
-			        if (nodes[i].id === base + '-checkbox-' + settings[j]) {
-				        nodes[i].checked = true;
-			        }
-		        }
-		        if (nodes[i].id === base + "-checkbox-" + settings[0]) {
-			        nodes[i].checked = false;
-			        var idx = settings.slice(1).indexOf(settings[0]);
-			        if (idx > -1) {
-				        // +1 and +2 b/c first-position item (primary) is sliced off for this check
-				        settings = settings.slice(0,idx + 1).concat(settings.slice(idx + 2));
-		                _io['citationLangPrefs'+name] = settings;
-			        }
-                    citationSetAffixes(nodes[i]);
-			        nodes[i].disabled = true;
-                } else if (radioClick && nodes[i].id === translitID) {
-                    // true invokes a quash of the affixes
-                    if (currentPrimaryID === translitID) {
-                        citationSetAffixes(nodes[i]);
-                    } else {
-                        citationSetAffixes(nodes[i], null, true);
-                    }
-                } else {
-			        nodes[i].disabled = false;
-		        }
-	        }
-        }
-    }
+		if (!settings || !settings[0]) {
+			settings = ['orig'];
+		}
+		var nodes = [];
+		var forms = ['orig', 'translit', 'translat'];
+		var base = name.toLowerCase();
+		// get node
+		// set node from pref
+		if (init) {
+			citationGetAffixes();
+			var currentPrimaryID = base + "-radio-" + settings[0];
+			var node = document.getElementById(currentPrimaryID);
+			var control = node.control;
+			control.selectedItem = node;
+			
+			var translitID = base + "-radio-translit";
+			var translitNode = document.getElementById(translitID);
+			nodes.push(translitNode);
+			
+			for (var i = 0, ilen = forms.length; i < ilen; i += 1) {
+				nodes.push(document.getElementById(base + "-checkbox-" + forms[i]));
+			}
+			for (var i = 0, ilen = nodes.length; i < ilen; i += 1) {
+				nodes[i].checked = false;
+				for (var j = 1, jlen = settings.length; j < jlen; j += 1) {
+					if (nodes[i].id === base + '-checkbox-' + settings[j]) {
+						nodes[i].checked = true;
+					}
+				}
+				if (nodes[i].id === base + "-checkbox-" + settings[0]) {
+					nodes[i].checked = false;
+					var idx = settings.slice(1).indexOf(settings[0]);
+					if (idx > -1) {
+						// +1 and +2 b/c first-position item (primary) is sliced off for this check
+						settings = settings.slice(0,idx + 1).concat(settings.slice(idx + 2));
+						_io['citationLangPrefs'+name] = settings;
+					}
+					citationSetAffixes(nodes[i]);
+					nodes[i].disabled = true;
+				} else if (radioClick && nodes[i].id === translitID) {
+					// true invokes a quash of the affixes
+					if (currentPrimaryID === translitID) {
+						citationSetAffixes(nodes[i]);
+					} else {
+						citationSetAffixes(nodes[i], null, true);
+					}
+				} else {
+					nodes[i].disabled = false;
+				}
+			}
+		}
+	}
 
-    function citationSetAffixes (node, affixNode, quashPrimaryAffixes) {
-        if (!node) {
-            var node = document.popupNode;
-        }
-        var currentId = node.id;
-        var prefixNode = document.getElementById(node.id + '-prefix');
-        var suffixNode = document.getElementById(node.id + '-suffix');
-        if (!affixNode || quashPrimaryAffixes) {
-            prefixNode.value = "";
-            suffixNode.value = "";
-        } else {
-            var prefix = affixNode.value.split("|")[0];
-            if (!prefix) {
-                prefix = "";
-            }
-            var suffix = affixNode.value.split("|")[1];
-            if (!suffix) {
-                suffix = "";
-            }
-            prefixNode.value = prefix;
-            suffixNode.value = suffix;
-        }
-        // Do something to store this data in Prefs
-        var types = ['persons', 'institutions', 'titles', 'journals', 'publishers', 'places'];
-	    var forms = ['orig', 'translit', 'translat'];
-        var affixList = [];
-        for (var i = 0, ilen = types.length; i < ilen; i += 1) {
-            affixListPush(types[i], "radio", "translit", affixList, "prefix");
-            affixListPush(types[i], "radio", "translit", affixList, "suffix");
-            for (var j = 0, jlen = forms.length; j < jlen; j += 1) {
-                affixListPush(types[i], "checkbox", forms[j], affixList, "prefix");
-                affixListPush(types[i], "checkbox", forms[j], affixList, "suffix");
-            }
-        }
+	function citationSetAffixes (node, affixNode, quashPrimaryAffixes) {
+		if (!node) {
+			var node = document.popupNode;
+		}
+		var currentId = node.id;
+		var prefixNode = document.getElementById(node.id + '-prefix');
+		var suffixNode = document.getElementById(node.id + '-suffix');
+		if (!affixNode || quashPrimaryAffixes) {
+			prefixNode.value = "";
+			suffixNode.value = "";
+		} else {
+			var prefix = affixNode.value.split("|")[0];
+			if (!prefix) {
+				prefix = "";
+			}
+			var suffix = affixNode.value.split("|")[1];
+			if (!suffix) {
+				suffix = "";
+			}
+			prefixNode.value = prefix;
+			suffixNode.value = suffix;
+		}
+		// Do something to store this data in Prefs
+		var types = ['persons', 'institutions', 'titles', 'journals', 'publishers', 'places'];
+		var forms = ['orig', 'translit', 'translat'];
+		var affixList = [];
+		for (var i = 0, ilen = types.length; i < ilen; i += 1) {
+			affixListPush(types[i], "radio", "translit", affixList, "prefix");
+			affixListPush(types[i], "radio", "translit", affixList, "suffix");
+			for (var j = 0, jlen = forms.length; j < jlen; j += 1) {
+				affixListPush(types[i], "checkbox", forms[j], affixList, "prefix");
+				affixListPush(types[i], "checkbox", forms[j], affixList, "suffix");
+			}
+		}
 		_io['citationAffixes'] = affixList;
-    }
+	}
 
-    function affixListPush(type, boxtype, form, lst, affix) {
-        var elem = document.getElementById(type + "-" + boxtype + "-" + form + "-" +affix);
-        if (!elem.value) {
-            elem.value = "";
-        }
-        lst.push(elem.value);
-    };
+	function affixListPush(type, boxtype, form, lst, affix) {
+		var elem = document.getElementById(type + "-" + boxtype + "-" + form + "-" +affix);
+		if (!elem.value) {
+			elem.value = "";
+		}
+		lst.push(elem.value);
+	};
 
-    function citationGetAffixes () {
+	function citationGetAffixes () {
 		var affixList = null;
 		if (_io['citationAffixes']) {
 			if (_io['citationAffixes'].length === 48) {
@@ -559,30 +559,30 @@ var Zotero_File_Interface_Bibliography = new function() {
 		if (!affixList) {
 			affixList = [,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,];
 		}
-        var types = ['persons', 'institutions', 'titles', 'journals', 'publishers', 'places'];
-	    var forms = ['orig', 'translit', 'translat'];
-        var count = 0;
-        for (var i = 0, ilen = types.length; i < ilen; i += 1) {
-            count =  citationGetAffixesAction(types[i], "radio", "translit", affixList, count);
-            
-            for (var j = 0, jlen = forms.length; j < jlen; j += 1) {
-                count = citationGetAffixesAction(types[i], "checkbox", forms[j], affixList, count);
-            }
-        }
-    }
+		var types = ['persons', 'institutions', 'titles', 'journals', 'publishers', 'places'];
+		var forms = ['orig', 'translit', 'translat'];
+		var count = 0;
+		for (var i = 0, ilen = types.length; i < ilen; i += 1) {
+			count =  citationGetAffixesAction(types[i], "radio", "translit", affixList, count);
+			
+			for (var j = 0, jlen = forms.length; j < jlen; j += 1) {
+				count = citationGetAffixesAction(types[i], "checkbox", forms[j], affixList, count);
+			}
+		}
+	}
 
-    function citationGetAffixesAction(type, boxtype, form, affixList, count) {
-        var affixPos = ['prefix', 'suffix']
-        for (var k = 0, klen = affixPos.length; k < klen; k += 1) {
-            var id = type + '-' + boxtype + '-' + form + '-' + affixPos[k];
-            var node = document.getElementById(id);
-            if (affixList[count]) {
-                node.value = affixList[count];
-        }
-            count += 1;
-        }
-        return count;
-    }
+	function citationGetAffixesAction(type, boxtype, form, affixList, count) {
+		var affixPos = ['prefix', 'suffix']
+		for (var k = 0, klen = affixPos.length; k < klen; k += 1) {
+			var id = type + '-' + boxtype + '-' + form + '-' + affixPos[k];
+			var node = document.getElementById(id);
+			if (affixList[count]) {
+				node.value = affixList[count];
+		}
+			count += 1;
+		}
+		return count;
+	}
 
 	function openProjectName (event) {
 		var node = event.target;
