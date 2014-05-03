6aab7c81deaf947f4d5e658399c92574eda21e0a
Frank Bennett <biercenator@gmail.com>
2014-05-03 09:39:03 +0900

    Tweak to allow sniffing for presence of a specific group library

--- chrome/content/zotero/xpcom/data/groups.js
+++ chrome/content/zotero/xpcom/data/groups.js
@@ -72,12 +72,16 @@ Zotero.Groups = new function () {
 	}
 	
 	
-	this.getLibraryIDFromGroupID = function (groupID) {
+	this.getLibraryIDFromGroupID = function (groupID, ignoreError) {
 		var sql = "SELECT libraryID FROM groups WHERE groupID=?";
 		var libraryID = Zotero.DB.valueQuery(sql, groupID);
 		if (!libraryID) {
-			throw ("Group with groupID " + groupID + " does not exist "
-					+ "in Zotero.Groups.getLibraryIDFromGroupID()");
+			if (ignoreError) {
+				libraryID = false;
+			} else {
+				throw ("Group with groupID " + groupID + " does not exist "
+					   + "in Zotero.Groups.getLibraryIDFromGroupID()");
+			}
 		}
 		return libraryID;
 	}
