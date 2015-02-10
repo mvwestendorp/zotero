e4f7644aa8ee4f3f7ffa1cb5045918580a0834f4
Frank Bennett <biercenator@gmail.com>
2015-02-10 21:24:06 +0900

    resource/schema/system.sql

--- resource/schema/system.sql
+++ resource/schema/system.sql
@@ -1,4 +1,4 @@
--- 10063
+-- 10064
 
 -- Copyright (c) 2009 Center for History and New Media
 --                    George Mason University, Fairfax, Virginia, USA
@@ -431,10 +431,10 @@ INSERT INTO itemTypeFields VALUES (3, 22, NULL, 24);
 INSERT INTO itemTypeFields VALUES (4, 110, NULL, 1);
 INSERT INTO itemTypeFields VALUES (4, 90, NULL, 2);
 INSERT INTO itemTypeFields VALUES (4, 12, NULL, 3);
-INSERT INTO itemTypeFields VALUES (4, 1284, NULL, 4);    -- status [ADDED]
-INSERT INTO itemTypeFields VALUES (4, 4, NULL, 5);
-INSERT INTO itemTypeFields VALUES (4, 5, NULL, 6);
-INSERT INTO itemTypeFields VALUES (4, 10, NULL, 7);
+INSERT INTO itemTypeFields VALUES (4, 4, NULL, 4);
+INSERT INTO itemTypeFields VALUES (4, 5, NULL, 5);
+INSERT INTO itemTypeFields VALUES (4, 10, NULL, 6);
+INSERT INTO itemTypeFields VALUES (4, 1284, NULL, 7);    -- status [ADDED]
 INSERT INTO itemTypeFields VALUES (4, 14, NULL, 8);
 INSERT INTO itemTypeFields VALUES (4, 3, NULL, 9);
 INSERT INTO itemTypeFields VALUES (4, 28, NULL, 10);
