a53f81222c6fd2567165cf27ee281394168fd72f
Frank Bennett <biercenator@gmail.com>
2015-05-30 20:21:36 +0900

    Use the right ID

--- test/runtests.sh
+++ test/runtests.sh
@@ -76,7 +76,7 @@ makePath ZOTERO_UNIT_PATH "$CWD"
 echo "$ZOTERO_UNIT_PATH" > "$PROFILE/extensions/zotero-unit@zotero.org"
 
 makePath ZOTERO_PATH "`dirname "$CWD"`"
-echo "$ZOTERO_PATH" > "$PROFILE/extensions/zotero@chnm.gmu.edu"
+echo "$ZOTERO_PATH" > "$PROFILE/extensions/juris-m@juris-m.github.io"
 
 # Create data directory
 mkdir "$PROFILE/zotero"
