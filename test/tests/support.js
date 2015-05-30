e5aabe88f382173fab1cb399acf8ab8c591116ff
Frank Bennett <biercenator@gmail.com>
2015-05-31 05:09:34 +0900

    Extend timeout (takes about 25000 on dev laptop)

--- test/tests/support.js
+++ test/tests/support.js
@@ -1,7 +1,7 @@
 describe("Support Functions for Unit Testing", function() {
 	describe("resetDB", function() {
 		it("should restore the DB to factory settings", function() {
-			this.timeout(10000);
+			this.timeout(50000);
 			var quickstart = Zotero.Items.erase(1);
 			assert.equal(Zotero.Items.get(1), false);
 			return resetDB().then(function() {
