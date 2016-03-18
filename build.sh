1febbb9efb9d8a002994d0ac8926e81e7c580c0a
fbennett <biercenator@gmail.com>
2016-03-18 22:01:28 +0900

    Fix path

--- build.sh
+++ build.sh
@@ -12,101 +12,112 @@ BRANCH="jurism"
 CLIENT="jurism"
 VERSION_ROOT="4.0.28.7m"
 
-# Error handlers
-. sh-lib/errors.sh
+function xx-remove-and-replace-old-build-dir () {
+    rm -fR build
+    for i in build build/styles build/translators; do
+        mkdir $i
+    done
+}
 
-# Setup
-. sh-lib/setup.sh
+function xx-save-aside-deleted-translators-list () {
+    if [ ${RELEASE} -gt 1 ]; then
+        cp translators/deleted.txt .
+    fi
+}
 
-# Version levels
-. sh-lib/versions.sh
+function xx-copy-files-into-xpi () {
+    for i in chrome components defaults resource; do
+        find $i -path "*/\.git" -prune -o -name "*~" -prune -o -print | zip "${XPI_FILE}" -@ >> "${LOG_FILE}"
+    done
 
-# Prompt for options
-#. sh-lib/prompt.sh
-
-# Parse command-line options
-. sh-lib/opts.sh
-
-# Functions for build
-. sh-lib/builder.sh
-
-# Functions for release
-. sh-lib/releases.sh
-
-# Functions for repo management
-. sh-lib/repo.sh
-
-if [ $RELEASE -gt 1 ]; then
-  DOORKEY=$(cat "${HOME}/bin/doorkey.txt")
-fi
-
-# Perform release ops
-case $RELEASE in
-    1)
-        echo "(1)"
-        # Preliminaries
-        increment-patch-level
-        if [ "$BETA" -gt 0 ]; then
-            increment-beta-level
+    for i in chrome.manifest COPYING deleted.txt install.rdf; do
+        if [ -f "$i" ]; then
+            echo $i | zip "${XPI_FILE}" -@ >> "${LOG_FILE}"
+        else
+            echo "Warning: file \"$i\" not found, unable to add to XPI" >> "${LOG_FILE}"
         fi
-        echo "Version: ${VERSION}"
+    done
 
-        # Build
-        echo "(a)"
-        touch-log
-        echo "(b)"
-        refresh-style-modules
-        echo "(c)"
-        build-the-plugin
-        echo "(d)"
-        repo-finish 1 "Built as ALPHA (no upload to GitHub)"
-        echo "(e)"
-        ;;
-    2)
-        echo "(2)"
-        # Claxon
-        check-for-uncommitted
-        # Preliminaries
-        increment-patch-level
-        increment-beta-level
-        save-beta-level
-        echo "Version is: $VERSION"
-        # Build
-        touch-log
-        refresh-style-modules
-        build-the-plugin
-        git-checkin-all-and-push
-        create-github-release
-        add-xpi-to-github-release
-        repo-finish 0 "Released as BETA (uploaded to GitHub, prerelease)"
-        ;;
-    3)
-        echo "(3)"
-        # Claxon
-        check-for-uncommitted
-        block-uncommitted
-        # Preliminaries
-        reset-beta-level
-        increment-patch-level
-        check-for-release-dir
-        save-patch-level
-        echo "Version is: $VERSION"
-        # Build
-        echo "A"
-        touch-log
-        echo "B"
-        refresh-style-modules
-        echo "C"
-        build-the-plugin
-        echo "D"
-        git-checkin-all-and-push
-        echo "E"
-        create-github-release
-        echo "F"
-        add-xpi-to-github-release
-        echo "G"
-        publish-update
-        echo "H"
-        repo-finish 0 "Released as FINAL (uploaded to GitHub, full wax)"
-        ;;
-esac
+    for i in $(find resource -type f); do
+        echo $i | zip "${XPI_FILE}" -@ >> "${LOG_FILE}"
+    done
+}
+
+function xx-zip-up-styles () {
+    cp styles/* build/styles
+
+    cd build/styles
+    zip styles *.csl >> "${LOG_FILE}"
+    cd ../..
+    mv build/styles/styles.zip .
+}
+
+function xx-zip-up-translators () {
+    if [ ${RELEASE} -gt 1 ]; then
+        if [ -f translators.index ]; then
+            rm translators.index
+        fi
+        touch translators.index
+
+        if [ -d translators_actual ]; then
+            rm -fR translators_actual
+        fi
+        mkdir translators_actual
+        cd translators_actual
+
+        wget -O translators_actual.zip http://citationstylist.org/translators/translators.zip
+        unzip translators_actual.zip >> "${LOG_FILE}"
+        cd ..
+
+        COUNT=0
+        for i in translators_actual/*.js; do
+            FILENAME=$(echo ${COUNT}.js)
+            KEY=$(grep -m 1 translatorID "$i"| sed -e "s/.*translatorID\" *: *\"\([^\"]\+\)\".*/\\1/")
+            NAME=$(grep -m 1 label "$i"| sed -e "s/.*label\" *: *\"\([^\"]\+\)\".*/\\1/")
+            DATE=$(grep -m 1 lastUpdated "$i"| sed -e "s/.*lastUpdated\" *: *\"\([^\"]\+\)\".*/\\1/")
+            if [ "${KEY}" != "" ]; then
+                echo ${FILENAME},${KEY},${NAME},${DATE} >> translators.index
+                cp "$i" build/translators/${FILENAME}
+                echo -n "${FILENAME} "
+                echo $((COUNT++)) > /dev/null
+            fi
+        done
+
+        cd build/translators
+        zip translators *.js >> "${LOG_FILE}"
+        cd ../..
+        cp build/translators/translators.zip .
+    fi
+}
+
+function xx-add-styles-and-translators-to-zip () {
+    echo styles.zip | zip "${XPI_FILE}" -@ >> "${LOG_FILE}"
+    echo translators.index | zip "${XPI_FILE}" -@ >> "${LOG_FILE}"
+    echo translators.zip | zip "${XPI_FILE}" -@ >> "${LOG_FILE}"
+}
+
+function xx-cleanup () {
+    rm styles.zip
+}
+
+function xx-patch-sources-before-build () {
+    "${SCRIPT_DIR}/tools/patch-chrome.sh"
+}
+
+function xx-unpatch-sources-after-build () {
+    "${SCRIPT_DIR}/tools/unpatch-chrome.sh"
+}
+
+function build-the-plugin () {
+    #xx-patch-sources-before-build
+    set-install-version
+    xx-remove-and-replace-old-build-dir
+    xx-save-aside-deleted-translators-list
+    xx-copy-files-into-xpi
+    xx-zip-up-styles
+    xx-zip-up-translators
+    xx-add-styles-and-translators-to-zip
+    xx-cleanup
+    #xx-unpatch-sources-after-build
+}
+. jm-sh/frontend.sh
