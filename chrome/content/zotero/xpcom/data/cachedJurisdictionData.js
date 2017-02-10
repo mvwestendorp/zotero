cc59f36abc24d84d99ade91c23216bcd4e8cbbce
fbennett <biercenator@gmail.com>
2017-02-10 10:57:30 +0900

    Set default jurisdiction

--- chrome/content/zotero/xpcom/data/cachedJurisdictionData.js
+++ chrome/content/zotero/xpcom/data/cachedJurisdictionData.js
@@ -77,7 +77,7 @@ Zotero.CachedJurisdictionData = new function() {
 	});
 	
 	this.setJurisdictionByIdOrName = Zotero.Promise.coroutine(function* (idOrName) {
-		var id = null;
+		var id = "default";
 		if (_jurisdictionNameToId[idOrName]) {
 			id = _jurisdictionNameToId[idOrName];
 		} else if (_jurisdictionIdToName[idOrName]) {
