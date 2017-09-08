32b9c33778d0f1c9d75ce1825259e871044395fc
Frank Bennett <biercenator@gmail.com>
2017-09-08 15:10:38 +0900

    Update citeproc-js

--- chrome/content/zotero/xpcom/citeproc.js
+++ chrome/content/zotero/xpcom/citeproc.js
@@ -9719,7 +9719,7 @@ CSL.NameOutput.prototype.fixupInstitution = function (name, varname, listpos) {
                 normalizedKey = this.state.sys.normalizeAbbrevsKey(long_form[j]);
             }
             jurisdiction = this.state.transform.loadAbbreviation(jurisdiction, "institution-part", normalizedKey);
-            if (this.state.transform.abbrevs[jurisdiction]["institution-part"][long_form[j]]) {
+            if (this.state.transform.abbrevs[jurisdiction]["institution-part"][normalizedKey]) {
                 short_form[j] = this.state.transform.abbrevs[jurisdiction]["institution-part"][normalizedKey];
                 use_short_form = true;
             }
