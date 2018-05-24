7e91e25fb5b0c988c833d5192c4e66c1e506dd26
Frank Bennett <biercenator@gmail.com>
2018-05-24 14:36:45 +0900

    Modify test to match Juris-M result

--- test/tests/zoteroTest.js
+++ test/tests/zoteroTest.js
@@ -38,12 +38,12 @@ describe("Zotero", function() {
 			
 			it("should add Zotero/[major.minor] to Chrome user agent", function () {
 				var ua = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.139 Safari/537.36';
-				assert.equal(Zotero.VersionHeader.update('example.com', ua, ZOTERO_CONFIG.CLIENT_NAME), ua + ` Zotero/${majorMinorVersion}`);
+				assert.equal(Zotero.VersionHeader.update('example.com', ua, ZOTERO_CONFIG.CLIENT_NAME), ua + ` Juris-M/${majorMinorVersion}`);
 			});
 				
 			it("should add Zotero/[major.minor] to Firefox user agent", function () {
 				var ua = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.13; rv:60.0) Gecko/20100101 Firefox/60.0';
-				assert.equal(Zotero.VersionHeader.update('example.com', ua, ZOTERO_CONFIG.CLIENT_NAME), ua + ` Zotero/${majorMinorVersion}`);
+				assert.equal(Zotero.VersionHeader.update('example.com', ua, ZOTERO_CONFIG.CLIENT_NAME), ua + ` Juris-M/${majorMinorVersion}`);
 			});
 		});
 	});
