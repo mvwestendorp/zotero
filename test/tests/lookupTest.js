de8f1e6f4f7c3a5a912e972e03092abd7bd84322
Frank Bennett <biercenator@gmail.com>
2019-04-02 15:54:45 +0900

    Extend timeout again for identifier tests.

--- test/tests/lookupTest.js
+++ test/tests/lookupTest.js
@@ -26,23 +26,23 @@ describe("Add Item by Identifier", function() {
 	// TODO: mock external services: https://github.com/zotero/zotero/issues/699
 	
 	it("should add an ISBN-10", function() {
-		this.timeout(20000);
+		this.timeout(40000);
 		return lookupIdentifier(win, "0838985890").then(function(ids) {
 			var item = Zotero.Items.get(ids[0]);
-			assert.equal(item.getField("title"), "Zotero: a guide for librarians, researchers, and educators");
+			assert.equal(item.getField("title"), "Zotero: a guide for librarians, researchers and educators");
 		});
 	});
 	
 	it("should add an ISBN-13", function() {
-		this.timeout(20000);
+		this.timeout(40000);
 		return lookupIdentifier(win, "978-0838985892").then(function(ids) {
 			var item = Zotero.Items.get(ids[0]);
-			assert.equal(item.getField("title"), "Zotero: a guide for librarians, researchers, and educators");
+			assert.equal(item.getField("title"), "Zotero: a guide for librarians, researchers and educators");
 		});
 	});
 	
 	it("should add a DOI", function() {
-		this.timeout(20000);
+		this.timeout(40000);
 		return lookupIdentifier(win, "10.4103/0976-500X.85940").then(function(ids) {
 			var item = Zotero.Items.get(ids[0]);
 			assert.equal(item.getField("title"), "Zotero: A bibliographic assistant to researcher");
@@ -55,7 +55,7 @@ describe("Add Item by Identifier", function() {
 	it.skip("should not add a PDF if a DOI already retrieves one");
 	
 	it("should add a PMID", function() {
-		this.timeout(20000);
+		this.timeout(40000);
 		return lookupIdentifier(win, "24297125").then(function(ids) {
 			var item = Zotero.Items.get(ids[0]);
 			assert.equal(item.getField("title"), "Taking control of your digital library: how modern citation managers do more than just referencing");
@@ -63,7 +63,7 @@ describe("Add Item by Identifier", function() {
 	});
 	
 	it("should add an item within a collection", function* () {
-		this.timeout(20000);
+		this.timeout(40000);
 		
 		var col = yield createDataObject('collection');
 		yield waitForItemsLoad(win);
